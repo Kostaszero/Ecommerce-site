eba26a82c04122e644ddd592d1d677b05125755f
nithin <b.nitin.naidu@gmail.com>
2023-09-18 23:53:18 +0530

    Updated Routing

--- breezy-ecommerce/src/app/app-routing.module.ts
+++ breezy-ecommerce/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { CheckOutComponent } from './check-out/check-out.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path: 'home', pathMatch: 'full', component: PolaroidListComponent},
+  {path: 'home', component: PolaroidListComponent},
   {path: 'cart-section', component: BagCartComponent},
   { path: 'check-out', component: CheckOutComponent },
   {path: '', pathMatch: 'full', redirectTo: 'home'},
